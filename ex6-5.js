45f9d2bf76f64f51fb8cab2cc46c21e603a6e3fe
yudaiyoshihara <zm18273@josai.ac.jp>
2019-11-19 03:27:48 +0000

    1119

--- ex6-5.js
+++ ex6-5.js
@@ -1,14 +1,14 @@
 var who65 = 'world';
 // var come = 'が来た'
-function sayhello65(){
+function sayhello65() {
 // var who = 'world'; 
-alert('Hello,'+who65 + '!');
+alert('Hello, ' + who65 + '!');
 }
 
-function taro65(){
-    who65='太郎';
+function taro65() {
+    who65 = '太郎';
     }
-function hanako65(){
-    who65='花子';
+function hanako65() {
+    who65 = '花子';
     }
 
